8608e843a25d3f5a9342ecfb7a072b244b9dded3
Marco Mark <marco@silvanite.com>
2017-07-18 21:58:13 +0100

    Enable access to the primary key which is no longer numeric

--- src/Permission.php
+++ src/Permission.php
@@ -14,6 +14,7 @@ class Permission extends Model
      * @var string
      */
     protected $primaryKey = 'permission_slug';
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
