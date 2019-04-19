4d8f0ac021a956eb896bb3b8ebbd0227bcf2e991
Bill Li <bill.li@ofcold.com>
2019-04-19 09:56:50 +0800

    Update brandenburg.php

--- src/Config/brandenburg.php
+++ src/Config/brandenburg.php
@@ -5,6 +5,9 @@ return [
      * User model class name.
      */
     'userModel' => env('USER_MODEL', 'App\User'),
+    
+    'user_table_name'   => 'users',
+    
 
     /**
      * Configure Brandenburg to not register its migrations.
